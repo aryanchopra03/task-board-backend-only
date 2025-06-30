83aa7ce17df835806d1138f2be0c8fa434c0598e
Aryan <aryanchopra320@gmail.com>
2025-06-30 15:52:46 +0530

    did cors configuration for frontend

--- task-board-backend/src/main/java/com/example/WebConfig.java
+++ task-board-backend/src/main/java/com/example/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public  void addCorsMappings (CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://task-board-frontend-2xs6.onrender.com")
                         .allowedMethods("GET","POST","PUT","DELETE")
                         .allowedHeaders("*");
             }
